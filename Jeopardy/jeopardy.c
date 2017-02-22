8ce247a65f210fc5394530dfa49a45d60fa59735
Wesley Lawrence <wesley.lawrence1@uoit.net>
2017-02-21 20:38:18 -0500

    changed so that the player name and score are in a struct

--- Jeopardy/jeopardy.c
+++ Jeopardy/jeopardy.c
@@ -8,6 +8,15 @@
 
 #define BUFFER_LEN 256
 
+struct players
+{
+	char* name;
+	int score;
+};
+
+
+int startScore = 0;
+
 int main(int argc, char const *argv[])
 {
 	int players;
@@ -32,10 +41,10 @@ int main(int argc, char const *argv[])
 		}
 	} while (players < 2 || players > 4);
 
-	char *name[players];
+	struct players playerSetup[players];
 	char playerName[BUFFER_LEN];
 	int count = 1;
-	int score[players];
+	
 
 	for (int i = 0; i < players; i++)
 	{
@@ -43,8 +52,8 @@ int main(int argc, char const *argv[])
 		scanf("%s", playerName);
 		printf("Welcome %s!\n", playerName);
 
-		name[i] = playerName;
-		score[i] = 0;
+		playerSetup[i].name = playerName;
+		playerSetup[i].score = startScore;
 		count++;
 	}
 
@@ -53,8 +62,8 @@ int main(int argc, char const *argv[])
 
 	startPlayer = rand() % players;
 	turn = startPlayer;
-	printf("%s starts.\n", name[turn]);
-	printf("%s, please enter in a category and price: ", name[turn]);
+	printf("%s starts.\n", playerSetup[turn].name);
+	printf("%s, please enter in a category and price: ", playerSetup[turn].name);
 
 
 //tokeizer doesnt even begin to work
