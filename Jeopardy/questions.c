5bae5f6eaa6fcf673e27c462100c43997c70cdbb
Wesley Lawrence <wesley.lawrence1@uoit.net>
2017-02-21 19:50:09 -0500

    now properly sets up the questions and answers for each category

--- Jeopardy/questions.c
+++ Jeopardy/questions.c
@@ -23,21 +23,17 @@ struct gameinfo
 
 char* gameCategories[3] = {"Celebrities", "Movies", "Games"};
 
-char* cat1Questions[4] = {"question1","question2","question3","question4"};
+char* cat1Questions[4] = {"q1","q2","q3","q4"};
 
-char* cat1Answers[4] = {"Answer1","Answer2","Answer3","Answer4"}; 
+char* cat1Answers[4] = {"a1","a2","a3","a4"}; 
 
-char* cat2Questions[4] = {"question1","question2","question3","question4"};
+char* cat2Questions[4] = {"q5","q6","q7","q8"};
 
-char* cat2Answers[4] = {"Answer1","Answer2","Answer3","Answer4"};
+char* cat2Answers[4] = {"a5","a6","a7","a8"};
 
-char* cat3Questions[4] = {"question1","question2","question3","question4"};
+char* cat3Questions[4] = {"q9","q10","q11","q12"};
 
-char* cat3Answers[4] = {"Answer1","Answer2","Answer3","Answer4"};
-
-char* cat4Questions[4] = {"question1","question2","question3","question4"};
-
-char* cat4Answers[4] = {"Answer1","Answer2","Answer3","Answer4"};
+char* cat3Answers[4] = {"a9","a10","a11","a12"};
 
 int questionValues[4] = {100, 200, 300, 400};
 
@@ -48,22 +44,38 @@ int wasQuestionAnswered[4] = {0, 0, 0, 0};
 void initialize_game(void)
 {
     // initialize each question struct and assign it to the questions array
-    struct gameinfo setup[12];
+    struct gameinfo setupC1[12];
+    struct gameinfo setupC2[12];
+    struct gameinfo setupC3[12];
     int i = 0;
 
 	for (int c = 0; c < 3; c++)
 	{
-		setup[i].questionCategory = gameCategories[c];
-		printf("%s\n",setup[i].questionCategory);
+		setupC1[i].questionCategory = gameCategories[c];
+		//printf("%s\n",setup[i].questionCategory);
 		for (int q = 0; q < 4; q++)
 		{
 			
-			setup[i].question = cat1Questions[q];
-			setup[i].answer = cat1Answers[q];
+			setupC1[i].question = cat1Questions[q];
+			setupC1[i].answer = cat1Answers[q];
 
-			printf("%s\n%s\n",setup[i].question, setup[i].answer);
+			setupC2[i].question = cat2Questions[q];
+			setupC2[i].answer = cat2Answers[q];
+
+			setupC3[i].question = cat3Questions[q];
+			setupC3[i].answer = cat3Answers[q];
+
+			
 			i++;
 		}
+	}
+
+
+	for (int j = 0; j < 4; j++)
+	{
+		printf("%s %s\n",setupC1[j].question, setupC1[j].answer);
+		printf("%s %s\n",setupC2[j].question, setupC2[j].answer);
+		printf("%s %s\n",setupC3[j].question, setupC3[j].answer);
 	}
 
     //printf("%s\n%s\n%s\n%i\n%i\n", jayZ.questionCategory, jayZ.question, jayZ.answer, jayZ.score, jayZ.isAnswered);
