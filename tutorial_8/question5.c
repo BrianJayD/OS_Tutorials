5a0f62335317f7171980d915114d42ade95ecd3f
JasdeepN <jasdeepn4@hotmail.com>
2017-03-19 12:15:23 -0400

    adding skeleton code for tutorial 8

---
+++ tutorial_8/question5.c
@@ -0,0 +1,16 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#include <pthread.h>
+#include <semaphore.h>
+#include <time.h>
+
+int main(){
+
+}
