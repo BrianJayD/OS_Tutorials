21890d449d0f8c55b5e613826214389874b66936
JasdeepN <jasdeepn4@hotmail.com>
2017-03-21 11:15:58 -0400

    completed question 1

--- tutorial_8/question1.c
+++ tutorial_8/question1.c
@@ -16,7 +16,6 @@ typedef	struct proc	{
 	char *name[256];
 	int priority;
 	int memory;
-	int val;
 } proc_t;
 
 typedef struct node {
@@ -29,6 +28,7 @@ node_t* init();
 void insert(node_t *head, proc_t *data);
 void print_tree(node_t *head);
 void printDFS(node_t *head);
+void read_file();
 
 node_t *proc_tree;
 
@@ -45,7 +45,7 @@ void insert(node_t *tree, proc_t *target) {
 	if (tree->data == NULL) {
 		tree->data = (proc_t*)malloc(sizeof(proc_t));
 		tree->data = target;
-	} else if (target->val < tree->data->val) {
+	} else if (target->priority < tree->data->priority) {
 		if (tree->left != NULL) {
 			insert(tree->left, target);
 		} else {
@@ -53,7 +53,7 @@ void insert(node_t *tree, proc_t *target) {
 			tree->left->data = (proc_t*)malloc(sizeof(proc_t));
 			tree->left->data = target;
 		}
-	} else if (target->val >= tree->data->val) {
+	} else if (target->priority >= tree->data->priority) {
 		if (tree->right != NULL) {
 			insert(tree->right, target);
 		} else {
@@ -66,11 +66,11 @@ void insert(node_t *tree, proc_t *target) {
 
 void print_tree(node_t * current) {
 	if (current != NULL)
-		printf("\n%d ", current->data->val);
+		printf("\nParent: %d, ", current->data->priority);
 	if (current->left != NULL)
-		printf("L%d ", current->left->data->val);
+		printf("L child: %d, ", current->left->data->priority);
 	if (current->right != NULL)
-		printf("R%d", current->right->data->val);
+		printf("R child: %d,", current->right->data->priority);
 	if (current->left != NULL)
 		print_tree(current->left);
 	if (current->right != NULL)
@@ -82,32 +82,64 @@ void printDFS(node_t * current) {
 	if (current->left != NULL)
 		printDFS(current->left);
 	if (current != NULL)
-		printf("%d ", current->data->val);
+		printf("%d ", current->data->priority);
 	if (current->right != NULL)
 		printDFS(current->right);
+}
+
+void read_file() {
+	FILE *process_file;
+	char line[256];
+
+	process_file = fopen("processes_tree.txt", "r");
+	if (process_file != NULL) {
+		char *pt;
+		while ( fgets (line, 256, process_file) != NULL) {
+			proc_t *temp = malloc(sizeof(proc_t));
+
+			pt = strtok (line, ",");
+			strcpy(temp->parent, pt);
+
+			pt = strtok (NULL, ",");
+			strcpy(temp->name, pt);
+
+			pt = strtok (NULL, ",");
+			temp->priority = atoi(pt);
+
+			pt = strtok (NULL, ",");
+			temp->memory = atoi(pt);
+
+			insert(proc_tree, temp);
+		}
+
+	} else {
+		printf("error opening processes_tree.txt for reading\n");
+		exit(1);
+	}
 }
 
 int main() {
 	proc_tree = init();
 
-	proc_t *temp = (proc_t*)malloc(sizeof(proc_t));
-	proc_t *temp1 = (proc_t*)malloc(sizeof(proc_t));
-	proc_t *temp2 = (proc_t*)malloc(sizeof(proc_t));
+	// proc_t *temp = (proc_t*)malloc(sizeof(proc_t));
+	// proc_t *temp1 = (proc_t*)malloc(sizeof(proc_t));
+	// proc_t *temp2 = (proc_t*)malloc(sizeof(proc_t));
 
 
-	temp->val = 1;
-	insert(proc_tree, temp);
-	temp1->val = 6;
-	insert(proc_tree, temp1);
-	temp2->val = 9;
-	insert(proc_tree, temp2);
-	
+	read_file();
+	// temp->val = 1;
+	// insert(proc_tree, temp);
+	// temp1->val = 6;
+	// insert(proc_tree, temp1);
+	// temp2->val = 9;
+	// insert(proc_tree, temp2);
 
-	printf("printing tree..\n");
+
+	printf("---\nprinting tree..\n---\n");
 	print_tree(proc_tree);
 	printf("\n");
 
-	printf("printing DFS\n");
+	printf("---\nprinting DFS\n---\n");
 	printDFS(proc_tree);
 	printf("\n");
 
